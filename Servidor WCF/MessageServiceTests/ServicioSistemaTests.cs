87c8778aa7a7c16e1e3586180b7eb79fc5d04757
IvanGutru <irving_cena2@hotmail.com>
2019-11-15 11:25:38 -0600

    Proyecto corriendo

---
+++ Servidor WCF/MessageServiceTests/ServicioSistemaTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MessageService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageService.Tests
+{
+    [TestClass()]
+    public class ServicioSistemaTests
+    {
+        [TestMethod()]
+        public void IniciarSesionTest(Cuenta cuenta)
+        {
+          //  Cuenta cuenta = new Cuenta();
+            cuenta.Contraseña = "";
+            cuenta.Correo = "";
+
+            ServicioSistema inicioSesion = new ServicioSistema();
+
+            var resultado = inicioSesion.IniciarSesion( cuenta);
+        }
+    }
+}
