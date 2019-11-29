561c9bc09660bb2f88b0d9a117254da9de4ff880
IvanGutru <irving_cena2@hotmail.com>
2019-11-29 14:17:43 -0600

    Primer analisis estatico de código
    Se disminuyó la deuda técnica de 5 horas a 1 hora

--- Cliente WPF/SerpientesEscaleras/RegistroUsuario.xaml.cs
+++ Cliente WPF/SerpientesEscaleras/RegistroUsuario.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace SerpientesEscaleras
 {
@@ -57,7 +47,7 @@ namespace SerpientesEscaleras
 
         }
         private Boolean ValidarCampos() {
-            var mensaje = Properties.Resources.mensajeValidacion;
+           
             if (textBox_NombreUsuario.Text == "" || textBox_Apellidos.Text =="" || textBox_Apodo.Text== "" || textBox_CorreoElectronico.Text ==""
                 || passwordBox_Contraseña.SecurePassword.Length == 0 || passwordBox_ConfirmarContraseña.SecurePassword.Length == 0) {
                 string camposObligatorios = Properties.Resources.camposObligatorios;
